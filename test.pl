147b19e3046ef7f9b123ead64cb97440a383ed35
Fabjan Sukalia <fsukalia@gmail.com>
2017-02-16 17:25:37 +0100

    test.pl: add qsort

--- test.pl
+++ test.pl
@@ -11,12 +11,13 @@ my $escp = "./uart_escape/uart_escape";
 
 # these testcases only generate output and don't need input
 my %tests = (
-	"hello", 200,
+	"hello", 400,
 	#"mandelbrot", 1500000000,
+	"qsort", 6000000,
 	"md5", 60000,
 	"md5_u", 60000,
-	"sha256", 70000,
-	#"sha256_u", 400, needs 17kB program memory
+	"sha256", 80000,
+	#"sha256_u", 80000, # only -Os works; needs 17kB program memory for -O2 and 18kB for -O3
 	"sha512", 200000,
 	#"sha512_u", 400 needs 59kB program memory
 );
@@ -25,7 +26,7 @@ my %io_tests = (
 	"echo", 4000000,
 	"calc", 4000000,
 	"lz4_comp", 20000000,
-	"lz4_dec",  3000000,
+	"lz4_dec",  4000000,
 );
 
 print "test       u c compression rate\n";
